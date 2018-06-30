de3ccc98e846e0edff0ad8df601399b20473df00
Ivan Babkin <ivan@babkin.ru>
2018-06-30 14:50:44 +0300

    init commit

---
+++ SimpleKnight/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SimpleKnight
+//
+//  Created by Ivan Babkin on 30.06.2018.
+//  Copyright © 2018 Ivan Babkin. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
