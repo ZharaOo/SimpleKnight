59c0a445fa1295be60fb73bc694fdcf7fd10780f
Ivan Babkin <ivan@babkin.ru>
2018-07-01 15:32:52 +0300

    добавлен finishview

---
+++ SimpleKnight/Finish/FinishView.swift
@@ -0,0 +1,33 @@
+//
+//  FinishView.swift
+//  SimpleKnight
+//
+//  Created by Ivan Babkin on 01.07.2018.
+//  Copyright © 2018 Ivan Babkin. All rights reserved.
+//
+
+import UIKit
+
+protocol FinishViewDelegate: class {
+    func goToMainMenu()
+}
+
+class FinishView: UIView {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    weak var delegate: FinishViewDelegate!
+    
+    class func instanceFromNib(score: Int, bestScore: Int) -> FinishView {
+        let fv = UINib(nibName: "RowView", bundle: nil).instantiate(withOwner: nil, options:nil)[0] as! FinishView
+        fv.frame = CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        fv.backgroundColor = UIColor.init(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
+        
+        fv.scoreLabel.text = "Score: \(score) \n\nBest: \(bestScore)"
+        
+        return fv
+    }
+    
+    @IBAction func mainMenu(_ sender: Any) {
+        delegate.goToMainMenu()
+    }
+}
